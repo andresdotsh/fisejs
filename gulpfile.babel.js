78fc5b7f26d8a831cc30619c8800dbd51419d9d3
Andrew Rosvel <rosvel@outlook.com>
2017-01-15 21:59:20 -0500

    Rename development assets directory

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,10 +8,10 @@ import pump from 'pump';
 // minify js task
 gulp.task('minjs', (cb) => {
   pump([
-    gulp.src('src/dev-assets/js/rv-file-server.js'),
+    gulp.src('src/assets-dev/js/rv-file-server.js'),
     concat('rv-file-server.min.js'),
     uglify(),
-    gulp.dest('src/dev-assets/js')
+    gulp.dest('src/assets-dev/js')
   ],
   cb);
 });
@@ -20,9 +20,9 @@ gulp.task('minjs', (cb) => {
 gulp.task('css', (cb) => {
   pump([
     gulp.src([
-      'src/dev-assets/css/bootstrap.min.css',
-      'src/dev-assets/css/font-awesome.min.css',
-      'src/dev-assets/css/rv-file-server.css'
+      'src/assets-dev/css/bootstrap.min.css',
+      'src/assets-dev/css/font-awesome.min.css',
+      'src/assets-dev/css/rv-file-server.css'
     ]),
     concat('styles.min.css'),
     cleanCss({
@@ -38,9 +38,9 @@ gulp.task('css', (cb) => {
 gulp.task('js', (cb) => {
   pump([
     gulp.src([
-      'src/dev-assets/js/jquery.min.js',
-      'src/dev-assets/js/bootstrap.min.js',
-      'src/dev-assets/js/rv-file-server.min.js'
+      'src/assets-dev/js/jquery.min.js',
+      'src/assets-dev/js/bootstrap.min.js',
+      'src/assets-dev/js/rv-file-server.min.js'
     ]),
     concat('scripts.min.js'),
     gulp.dest('src/assets/js')
@@ -53,7 +53,7 @@ gulp.task('eslint', () => {
   gulp
     .src([
       'src/*.js',
-      'src/dev-assets/js/rv-file-server.js',
+      'src/assets-dev/js/rv-file-server.js',
       'src/controllers/*.js',
       'src/routes/*.js'
     ])
